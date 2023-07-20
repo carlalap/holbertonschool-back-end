e0e21d1289fde2734d645e71c58d359693636a53
carlalap <carlalap>
2023-07-20 14:37:14 -0700

    dictionary

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -10,24 +10,33 @@ import sys
 from sys import argv
 
 if __name__ == "__main__":
-    url_users = "https://jsonplaceholder.typicode.com/users/{}"
+    get_id = set()
+    request = requests.get("https://jsonplaceholder.typicode.com/posts")
+    data = request.json()
+
+    for user in data:
+        get_id.add(user.get("userId"))
+    file_export = {}
+    url_u = "https://jsonplaceholder.typicode.com/users/{}"
     url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"
 
-    url = requests.get(url_users.format(argv[1]))
-    data_name = url.json().get('username')
+    for user in get_id:
+        url_users = request.get(url_u.format(user))
+        url_name = url_users.json().get("username")
+        url_users = request.get(url_todos.format(user))
+        data_request = url_users.json()
+        file_export['{}'.format(user)] = []
 
-    url = requests.get(url_todos.format(argv[1]))
-    data = url.json()
-
-    file_export = {}
-    file_export["{}".format(argv[1])] = []
+        for task in data_request:
+            file_export['{}'.format(user)].append()
 
     for task in data:
         file_export["{}".format(argv[1])].append({
             "task": task.get("title"),
             "completed": task.get("completed"),
-            "username": data_name
+            "username": url_name
         })
 
-    with open("{}.json".format(argv[1]), mode='w') as file_json:
-        json.dump(file_export, file_json)
+    with open('todo_all_employees.json', mode='w') as file:
+        json.dump({int(x): file_export[x] for x in file_export.keys()},
+                   file, sort_keys=True)
