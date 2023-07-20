946a07392da6d35949709e5bb6e3b66634c3217a
carlalap <carlalap>
2023-07-20 11:26:55 -0700

    PEP8 gather

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Python script that uses a REST API 
+Python script that uses a REST API
 (https://jsonplaceholder.typicode.com/), for a given
 employee ID, returns information about his/her TODO list progress
 """
-
+import json
 import requests
 import sys
-import json
+
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
@@ -15,7 +15,7 @@ def get_employee_todo_progress(employee_id):
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
     try:
-        # define the base URL of the REST API 
+        # define the base URL of the REST API
         employee_response = requests.get(employee_url)
         todo_response = requests.get(todo_url)
 
@@ -29,7 +29,8 @@ def get_employee_todo_progress(employee_id):
         done_tasks = [task for task in todo_data if task["completed"]]
         total_tasks = len(todo_data)
 
-        print(f"Employee {employee_name} is done with tasks({len(done_tasks)}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks
+              ({len(done_tasks)}/{total_tasks}): ")
 
         for task in done_tasks:
             print(f"\t{task['title']}")
@@ -43,6 +44,7 @@ def get_employee_todo_progress(employee_id):
     except ValueError:
         print("Error: Invalid JSON response.")
         sys.exit(1)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
