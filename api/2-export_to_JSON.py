d2e4fcaabfd35752878f7da51ea291b7c01906cd
carlalap <carlalap>
2023-07-20 13:59:47 -0700

    2. Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,47 +7,22 @@ to export data in the JSON format.
 import json
 import requests
 import sys
+from sys import argv
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com"
-    users_url = f"{base_url}/users"
-    todos_url = f"{base_url}/todos"
+        url_users = "https://jsonplaceholder.typicode.com/users/{}"
+        url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"
+        url = requests.get(url_users.format(argv[1]))
+        data_name = url.json().get('username')
+        url = requests.get(url_todos.format(argv[1]))
+        data = url.json()
+        file_export = {}
+        file_export["{}".format(argv[1])] = []
+        for task in data:
+                file_export["{}".format(argv[1])].append({
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": data_name})
 
-    try:
-        users_response = requests.get(users_url)
-        users_response.raise_for_status()
-        users_data = users_response.json()
-
-        all_employees_tasks = {}
-
-        for user in users_data:
-            user_id = user["id"]
-            username = user["username"]
-
-            todos_response = requests.get(todos_url,
-                                          params={"userId": user_id})
-            todos_response.raise_for_status()
-            todos_data = todos_response.json()
-
-            user_tasks = []
-            for task in todos_data:
-                task_title = task["title"]
-                task_completed = task["completed"]
-
-                user_tasks.append({
-                    "username": username,
-                    "task": task_title,
-                    "completed": task_completed
-                })
-
-            all_employees_tasks[user_id] = user_tasks
-
-        with open("todo_all_employees.json", mode="w") as file_json:
-            json.dump(all_employees_tasks, file_json)
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-    except ValueError:
-        print("Error: Invalid JSON response.")
-        sys.exit(1)
+        with open("{}.json".format(argv[1]), mode="w") as file_json:
+                json.dump(file_export, file_json)
