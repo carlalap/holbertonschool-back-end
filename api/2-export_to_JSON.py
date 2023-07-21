ef268a53ac117ea74a5080d89d8f69b5b4b3a6d5
carlalap <carlalap>
2023-07-21 12:21:44 -0700

    2-export

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,43 +1,28 @@
 #!/usr/bin/python3
-""" Export to JSON """
+"""
+Python script that uses a REST API
+(https://jsonplaceholder.typicode.com/),
+to export data in the JSON format.
+"""
 import json
 import requests
 import sys
-
-
-def export_user_to_json():
-    """export user id data to json format"""
-
-    USER_ID = sys.argv[1]
-
-    user_data = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                             .format(USER_ID)).json()
-    todo_data = requests.get('https://jsonplaceholder.typicode.com/todos',
-                             params={"userId": USER_ID}).json()
-    EMPLOYEE_UN = user_data.get("username")
-    EMPLOYEE_NAME = user_data.get("name")
-
-    counter = 0
-    task_list = []
-    complete_status_list = []
-    for thing in todo_data:
-        counter += 1
-        task_list.append(thing.get("title"))
-        complete_status_list.append(thing.get("completed"))
-
-    json_list = []
-    for i in range(counter):
-        new_dict = {
-            "task": task_list[i],
-            "completed": complete_status_list[i],
-            "username": EMPLOYEE_UN
-        }
-        json_list.append(new_dict)
-
-    json_dict = {
-        f"{USER_ID}": json_list
-    }
-
+from sys import argv
 
 if __name__ == "__main__":
-    export_user_to_json()
+        url_users = "https://jsonplaceholder.typicode.com/users/{}"
+        url_todos = "https://jsonplaceholder.typicode.com/todos?userId={}"
+        url = requests.get(url_users.format(argv[1]))
+        data_name = url.json().get('username')
+        url = requests.get(url_todos.format(argv[1]))
+        data = url.json()
+        file_export = {}
+        file_export["{}".format(argv[1])] = []
+        for task in data:
+                file_export["{}".format(argv[1])].append({
+                            "task": task.get("title"),
+                            "completed": task.get("completed"),
+                            "username": data_name})
+
+        with open("{}.json".format(argv[1]), mode="w") as file_json:
+                json.dump(file_export, file_json)
